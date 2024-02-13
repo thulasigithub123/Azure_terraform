06f298a963767b92c30d32c411ef7e850b236d4d
thulasigithub123 <thulasiraman288@gmail.com>
2024-02-13 21:02:25 +0530

    variables

---
+++ structured_syntax/README.md
@@ -0,0 +1,4 @@
+1. create backend.tf
+2. create provider.tf
+3. create variables.tf
+4.
---
+++ structured_syntax/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+    description="the azure region in which all the resources in this example should be created"
+
+}
+
+variable "resource_group_name"{
+
+}
+
+variable "storage_account_name"{
+
+}
+
+variable "source_content"{
+    
+}
